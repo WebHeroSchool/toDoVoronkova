2600c6f3c435b7230310f01e4123516a9c522126
Анастасия Воронкова <voronkova__ab@mail.ru>
2021-08-03 14:59:36 +0500

    Удалила ненужные файлы и выполнила задание

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import { count, length } from './numbers';
+import React from 'react';
 
-const hello = 'Hello';
-const sum = 34 + 50;
-const number = 18;
-const checkBox = true;
-const flag = false;
+const element = (
+  <div>
+    Hello World!
+  </div>
+);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{`count * length = ${count * length}`}</p>
-        <p style={{
-          'color': 'green',
-          'border': '1px solid #fff',
-          'border-radius': 10 
-        }}>{hello}</p>
-        <p>{number}</p>
-        <p>{sum}</p>
-        <p>{checkBox && 'checkBox is true'}</p>
-        <p>{flag ? 'flag is true' : 'flag is false'}</p>
-        <p>{undefined}</p>
-        <p>{null}</p>
-        <p>{true}</p>
-        <p>{false}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { element };
