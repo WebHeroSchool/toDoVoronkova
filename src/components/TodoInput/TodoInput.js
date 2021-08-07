9677390fa4be54b5063ab90291d8187f72d260b4
Анастасия Воронкова <voronkova__ab@mail.ru>
2021-08-07 14:48:22 +0500

    добавила поле ввода и кнопку из библиотеки

--- src/components/TodoInput/TodoInput.js
+++ src/components/TodoInput/TodoInput.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const TodoInput = () => (<form>
-  <input type='text'></input>
-</form>);
+const TodoInput = () => (<div>
+      <TextField
+          id="standard-textarea"
+          placeholder="Введите задачу"
+          multiline
+        />
+        <Button color="primary">Добавить</Button>
+</div>);
 
 export default TodoInput;
