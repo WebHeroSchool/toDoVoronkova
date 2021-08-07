5646ed0f3adeb03ea2e9e0b08b50422b77da4383
Анастасия Воронкова <voronkova__ab@mail.ru>
2021-08-07 14:09:12 +0500

    Добавила условие, по которому будет меняться внешний вид элемента списка

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import classnames from 'classnames';
+import styles from './TodoItem.module.css';
 
-const TodoItem = ({ text }) => (<span>{ text }</span>);
+const TodoItem = ({ text, isDone }) => (<span className= {
+  classnames({
+    [styles.item]: true,
+    [styles.done]: isDone
+  })
+}>{ text }</span>);
 
 export default TodoItem;
