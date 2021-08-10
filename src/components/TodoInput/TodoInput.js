cc5c6977b63dafd2fffcc4f156d1dfe0191e8bea
Анастасия Воронкова <voronkova__ab@mail.ru>
2021-08-10 19:29:09 +0500

    Прописала стили приложения

--- src/components/TodoInput/TodoInput.js
+++ src/components/TodoInput/TodoInput.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import styles from "./TodoInput.module.css";
 
-const TodoInput = () => (<form>
-  <input type='text'></input>
-</form>);
+const TodoInput = () => (
+  <form className={styles.form}>
+    <input
+      type="text"
+      class={styles.input}
+      placeholder="What needs to be done?"
+    />
+
+    {/* //кнопка будет появляться, когда что-то написали в форме */}
+    <button type="button" class={styles.btn}>
+      +
+    </button>
+  </form>
+);
 
 export default TodoInput;
